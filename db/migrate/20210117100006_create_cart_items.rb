5e49e644d6b6003f31969c6cddd41f5eeefa93a4
sukepio <naoto927sukena@gmail.com>
2021-01-18 06:31:04 +0000

    first commit

---
+++ db/migrate/20210117100006_create_cart_items.rb
@@ -0,0 +1,12 @@
+class CreateCartItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cart_items do |t|
+      t.integer :item_id, null: false
+      t.integer :customer_id, null: false
+      t.integer :amount, null: false
+      t.timestamps
+    end
+    
+    add_index :cart_items, :id,                unique: true
+  end
+end
---
+++ db/migrate/20210117100032_create_deliveries.rb
@@ -0,0 +1,13 @@
+class CreateDeliveries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :deliveries do |t|
+      t.integer :customer_id, null: false	
+      t.string :name, null: false	
+      t.string :postcode, null: false	
+      t.string :address, null: false
+      t.timestamps
+    end
+    
+    add_index :deliveries, :id,                unique: true
+  end
+end
